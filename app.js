76bf7a4e6708daefa406663f69eaa15e0a4138e6
MarinaRH <l_marina1717@hotmail.com>
2018-05-05 20:19:20 -0500

    Obteniendo el numero mayor

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-alert('hola');
+
+var btnMax = document.getElementById("btn-max");
+var maxNum = document.getElementById("maxNum");
+
+btnMax.addEventListener('click',function() {
+    
+    var array = [45,75,200,256,1,27];
+    maxNum.value = Math.max.apply(true,array);
+    // console.log(maxNum);
+
+});
