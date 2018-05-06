65a2287b070e05b31e2b2fbe1efad89369916ca0
MarinaRH <l_marina1717@hotmail.com>
2018-05-06 18:30:48 -0500

    Ejercicio 1c

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
-// ejercicio 1a
-
-var btnMax = document.getElementById("btn-max"),
-maxNum = document.getElementById("maxNum");
+var btnMax = document.getElementById("btn"),
+maxNum = document.getElementById("maxNum"),
+minNum = document.getElementById("minNum"),
+secondNum = document.getElementById("secondNum"),
+array = [45,75,200,256,1,27];
 
 btnMax.addEventListener('click',function() {
-    
-    var array = [45,75,200,256,1,27];
-    maxNum.value = Math.max.apply(true,array);
-    // console.log(maxNum);
-
-});
+// ejercicio 1a
+    var mayor = 0;
+    for(i = 0; i < array.length; i++){
+        if (array[i] > mayor)
+        {
+            mayor = array[i];
+        }
+    }
+    maxNum.value = mayor;
 
 // ejercicio 1b
-
-var btnMin = document.getElementById("btn-min"),
-minNum = document.getElementById("minNum");
-
-btnMin.addEventListener('click',function() {
-
-    var array = [45,75,200,256,1,27];
-    minNum.value = Math.min(...array);
-
+    var menor = mayor;
+    for(i = 0; i < array.length; i++){
+        if (array[i] < menor)
+        {
+            menor = array[i];
+        }
+    }
+    minNum.value = menor;
+    
+// ejercicio 1c
+    var second=0;
+    for(i = 0; i < array.length; i++){
+        if (array[i] > second & array[i] < mayor)
+        {
+            second = array[i];
+        }
+    }
+    secondNum.value = second;
 });
