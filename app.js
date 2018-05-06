469be0755921aae08e887ff155cae108bf91913d
MarinaRH <l_marina1717@hotmail.com>
2018-05-05 20:25:02 -0500

    Obteniendo el numero menor

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+// ejercicio 1a
 
-var btnMax = document.getElementById("btn-max");
-var maxNum = document.getElementById("maxNum");
+var btnMax = document.getElementById("btn-max"),
+maxNum = document.getElementById("maxNum");
 
 btnMax.addEventListener('click',function() {
     
     var array = [45,75,200,256,1,27];
     maxNum.value = Math.max.apply(true,array);
     // console.log(maxNum);
+
+});
+
+// ejercicio 1b
+
+var btnMin = document.getElementById("btn-min"),
+minNum = document.getElementById("minNum");
+
+btnMin.addEventListener('click',function() {
+
+    var array = [45,75,200,256,1,27];
+    minNum.value = Math.min(...array);
 
 });
