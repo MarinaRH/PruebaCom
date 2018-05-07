c605e9f300dfdbc28e21ddf14f81f149fe5c05eb
MarinaRH <l_marina1717@hotmail.com>
2018-05-06 22:00:12 -0500

    Ejercicios 2a

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-var btnMax = document.getElementById("btn"),
+var btn = document.getElementById("btn"),
+btn2 = document.getElementById("btn2"),
 maxNum = document.getElementById("maxNum"),
 minNum = document.getElementById("minNum"),
 secondNum = document.getElementById("secondNum"),
-array = [45,75,200,256,1,27];
+nameLarge = document.getElementById("large"),
+array = [45,75,200,256,1,27],
+arreglo = ['erik','leonardo','andres','gerardo','jan','renzo'];
 
-btnMax.addEventListener('click',function() {
+btn.addEventListener('click',function() {
 // ejercicio 1a
     var mayor = 0;
     for(i = 0; i < array.length; i++){
@@ -34,4 +37,25 @@ btnMax.addEventListener('click',function() {
         }
     }
     secondNum.value = second;
+});
+
+// ejercicio2
+
+btn2.addEventListener('click',function() {
+    
+    var large = 0;
+    
+    for(i = 0; i < arreglo.length; i++){
+
+    for(j = 0; j < arreglo[i].length; j++){
+        
+        if (arreglo[i].length > large)
+        {
+            large = arreglo[i].length;
+            var name = arreglo[i];
+        }
+    }
+
+    }
+    nameLarge.value= name;
 });
